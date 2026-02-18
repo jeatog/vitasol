7180e1068ab3023186f1f76c1a3ae8f33b040094
Jesús A. Toledo <jeatog@hotmail.com>
2026-02-18 11:10:23 -0600

    Initial Commit

---
+++ Vitasol/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vitasol
+//
+//  Created by Jesús Toledo on 18/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
